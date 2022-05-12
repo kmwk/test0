72462d562920c1dccac76f5c4f893fbcdb3ba149
kmwk <minwook032@gmail.com>
2022-05-13 02:22:43 +0900

    test0

---
+++ github_test0/소스.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+
+int return_result(int b, int b1)
+{
+	int c = b * b1;
+	c++;
+	return c;
+}
+
+int main() 
+{
+	int a = 3;
+	int a1 = 2;
+	
+	printf("%d", return_result(a, a1));
+
+}
+
